a802ce1265f4efb6c3411e74bd1339690826619b
Chenbo Yi <yichenbo@outlook.com>
2019-06-06 22:54:19 +0100

    refactor: extra the conditon statement for Aged Brie

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,48 +7,46 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros'){
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.alterItemQuality(this.items[i])
       }
     }
     return this.items;
   }
 
-  alterItemQuality(item){
+  alterItemQuality(item) {
     item.sellIn = item.sellIn - 1;
 
-    if(item.quality > 0 && item.quality < 50){
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            item.quality = item.quality - 1;
-      } else {
+    if (item.quality > 0 && item.quality < 50) {
+      if (item.name == 'Aged Brie') {
+        item.quality = item.quality + 1;
+      } else {    
+        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          item.quality = item.quality - 1;
+        } else {
           item.quality = item.quality + 1;
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-                item.quality = item.quality + 1;
+              item.quality = item.quality + 1;
             }
             if (item.sellIn < 6) {
               item.quality = item.quality + 1;
             }
           }
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
+        }
+        if (item.sellIn < 0) {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-              item.quality = item.quality - 1;
+            item.quality = item.quality - 1;
           } else {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
           }
-        } else {
-          item.quality = item.quality + 1;
         }
       }
     }
   }
-
-
 }
