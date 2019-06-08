76eb3ffee72e54e8dca32228fa9eceadfb3705dc
Chenbo Yi <yichenbo@outlook.com>
2019-06-08 20:38:30 +0100

    Refactor: extract legendary item from updateItem

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,59 +6,81 @@ class Item {
   }
 }
 
+
+
 class Shop {
   constructor(items = []) {
     this.items = items;
+    this.specialItems = ['Sulfuras, Hand of Ragnaros', 'Aged Brie',
+      'Backstage passes to a TAFKAL80ETC concert',
+      'Conjured Items'
+    ]
   }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.alterItemQuality(this.items[i])
-      }
+        this.logItemQuality(this.items[i])
     }
     return this.items;
   }
 
-  alterItemQuality(item) {
-    item.sellIn --;
-
+  logItemQuality(item) {
+    if(item.name == 'Sulfuras, Hand of Ragnaros' ){
+      return item;
+    };
     if (item.quality > 0 && item.quality < 50) {
-      if (item.name == 'Aged Brie') {
-        item.quality ++;
-      } else {
-        if(item.sellIn >= 0){
-          this.changeItemQualityBeforeSale(item)
-        } else {
-          this.changeItemQualityAfterSale(item)
-        }
-      }
-    }
+      this.updateItem(item)
+    };
   }
 
-  changeItemQualityBeforeSale(item){
+
+  updateItem(item){
+    item.sellIn--;
+
+    if (item.name == 'Aged Brie') {
+      item.quality++;
+    } else {
+      if (item.sellIn >= 0) {
+        this.changeItemQualityBeforeSale(item)
+      } else {
+        this.changeItemQualityAfterSale(item)
+      }
+    };
+  }
+
+
+
+
+
+  changeItemQualityBeforeSale(item) {
     if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-      if(item.sellIn > 10){
-        item.quality ++;
-      } else if (item.sellIn > 5 && item.sellIn <= 10){
+      if (item.sellIn > 10) {
+        item.quality++;
+      } else if (item.sellIn > 5 && item.sellIn <= 10) {
         item.quality += 2;
       } else {
         item.quality += 3;
       }
-    } else if(item.name == 'Conjured Items'){
+    } else if (item.name == 'Conjured Items') {
       item.quality -= 2;
-    }
-    else {
-      item.quality --;
+    } else {
+      item.quality--;
     }
   }
 
-  changeItemQualityAfterSale(item){
+  changeItemQualityAfterSale(item) {
     if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
       item.quality = 0;
-    } else if(item.name == 'Conjured Items'){
+    } else if (item.name == 'Conjured Items') {
       item.quality -= 4;
     } else {
       item.quality -= 2;
     }
   }
+
+
+
+
+
+
 }
