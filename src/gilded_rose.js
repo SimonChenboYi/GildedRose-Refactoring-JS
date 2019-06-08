10175d81ce50e8b9e260148dc3e0283339f73d91
Chenbo Yi <yichenbo@outlook.com>
2019-06-08 21:45:04 +0100

    refactor: remove the hard coded item names from methods

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,15 +6,15 @@ class Item {
   }
 }
 
-
-
 class Shop {
   constructor(items = []) {
     this.items = items;
-    this.specialItems = ['Sulfuras, Hand of Ragnaros', 'Aged Brie',
-      'Backstage passes to a TAFKAL80ETC concert',
-      'Conjured Items'
-    ]
+    this.specialItems = {
+      legendaryItems: ['Sulfuras, Hand of Ragnaros'],
+      agedItems: ['Aged Brie'],
+      backstagePasses: ['Backstage passes to a TAFKAL80ETC concert'],
+      conjuredItems: ['Conjured Items']
+    }
   }
 
   updateQuality() {
@@ -25,19 +25,15 @@ class Shop {
   }
 
   logItemQuality(item) {
-    if (item.name == 'Sulfuras, Hand of Ragnaros') return item;
-
-    if (item.quality > 0 && item.quality < 50) {
-      this.updateItem(item)
-      return item;
-    };
+    if (this.isLegendaryItem(item)) return item;
+    if (item.quality > 0 && item.quality < 50) return this.updateItem(item)
   }
 
   updateItem(item) {
     item.sellIn--;
-    if (item.name == 'Aged Brie') return this.updateAgedBrie(item);
-    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') return this.updateBackstagePasses(item);
-    if (item.name == 'Conjured Items') return this.updateConjuredItems(item);
+    if (this.isAgedItem(item)) return this.updateAgedBrie(item);
+    if (this.isBackstagePass(item)) return this.updateBackstagePasses(item);
+    if (this.isConjuredItem(item)) return this.updateConjuredItems(item);
     this.updateNormalItems(item);
     return item;
   }
@@ -48,23 +44,39 @@ class Shop {
   }
 
   updateBackstagePasses(item) {
-    if (item.sellIn > 10) item.quality ++;
+    if (item.sellIn > 10) item.quality++;
     if (item.sellIn > 5 && item.sellIn <= 10) item.quality += 2;
     if (item.sellIn >= 0 && item.sellIn <= 5) item.quality += 3;
-    if (item.sellIn < 0 ) item.quality = 0;
+    if (item.sellIn < 0) item.quality = 0;
     return item;
   }
 
   updateConjuredItems(item) {
     if (item.sellIn >= 0) item.quality -= 2;
-    if (item.sellIn < 0 ) item.quality -= 4;
+    if (item.sellIn < 0) item.quality -= 4;
     return item;
   }
 
   updateNormalItems(item) {
     if (item.sellIn >= 0) item.quality -= 1;
-    if (item.sellIn < 0 ) item.quality -= 2;
+    if (item.sellIn < 0) item.quality -= 2;
     return item;
+  }
+
+  isLegendaryItem(item){
+    return this.specialItems.legendaryItems.includes(item.name)
+  }
+
+  isAgedItem(item){
+    return this.specialItems.agedItems.includes(item.name)
+  }
+
+  isBackstagePass(item){
+    return this.specialItems.backstagePasses.includes(item.name)
+  }
+
+  isConjuredItem(item){
+    return this.specialItems.conjuredItems.includes(item.name)
   }
 
 }
